36aabed37ff4800e9a4393aefd94f43c1decaa8c
System Administrator <root@Mohameds-MacBook-Air.local>
2018-06-25 16:24:43 -0400

    React components setup

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div>Home Page</div>
+            
+        )
+    }
+}
+
+
+export default Home;
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path')
 const app = express();
 // const db = require ('./server/database/connection.js');
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
