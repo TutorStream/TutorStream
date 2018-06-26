843e28f7f48b6a6814cc66554bd544f274687451
Michael Cortez <mijcortez@gmail.com>
2018-06-26 19:51:18 -0400

    session queries

--- server/controllers/sessions.js
+++ server/controllers/sessions.js
@@ -1,24 +1,33 @@
 // import Session model
 // const Session = require('./../../database/Session') // INSERT ACTUAL FILE NAME
+const Session = require('./models/sessionModel')
 
 exports.bookSession = (req, res) => {
-  const testId = req.body.testId;
-  const userId = req.body.userId;
-  const tutorId = req.body.tutorId;
-  const date = req.body.date;
-  const duration = req.body.duration;
+  Session.bookSessionDB(req.body, (err, result) => {
+    if(err) {
+      console.log(err)
+    } else {
+      res.status(201).end()
+    }
+  })
   // use Session model to add a new session with tutor 
   // send back all requisite session info (id, test id, tutor id, userId, date, duration) to client
 };
 
 exports.deleteSession = (req, res) => {
-  var sessionId = req.body.sessionId;
   // use session model to delete session
   // send back 201 to client
+  Session.deleteSessionDB(req.body, (err, result) => {
+    if (err)  {
+      console.log(err)
+    } else {
+      res.status(201).end()
+    }
+  })
 };
 
 exports.changeSession = (req, res) => {
-  var sessionId = req.body.sessionId;
+  var sessionId = req.body
   // use session model to update a session
   // send back 201 to  client
 ;}
