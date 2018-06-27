fc809f74c0e755ba927fcdbc5d307ee03e1e78ad
Michael Cortez <mijcortez@gmail.com>
2018-06-27 12:10:13 -0400

    session logic without db is setup

---
+++ client/src/components/student/Sessions.jsx
@@ -0,0 +1,39 @@
+import React, {Component} from 'react'
+
+
+class Sessions extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            sessions: []
+        }
+    }
+    componentDidMount(){
+        this.getUpcomingSessions()
+    }
+    getUpcomingSessions() {
+        // id will come from state or this.props.match.params
+        console.log(`Axios request to get upcoming sessions`)
+        // const {id} = this.props.match.params
+        // axios.get(`/sessions/${id}`)
+        // .then((resp) => {
+        //     this.setState({
+        //         sessions: resp.data
+        //     })
+        // })
+    }
+    render() {
+        return (
+            <div>
+                <h1>Session Component!</h1> 
+                <ul>
+                {this.state.sessions.map((info, index) => {
+                    return (<li key={index} >{info.date}</li>)
+                })}
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Sessions 
