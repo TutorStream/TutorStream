0c58f84cbd42b50ce71ebf704037df7e2d7d169b
Nicholas Boylan <nboylan1pc@gmail.com>
2018-06-27 14:29:46 -0400

    Complete user controller and model helpers

--- server/controllers/tests.js
+++ server/controllers/tests.js
@@ -1,5 +1,4 @@
-// import Test model
-// const Test = require('./../../database/Test') // INSERT ACTUAL FILE NAME
+const Test = require('./../models/testModel')
 
 exports.getAllTests = (req, res) => {
   // get all tests, nothing in req body, just run get all tests from DB helpers
