8274138d5106bf4a4233e3dba137f33a6d9847e7
Nicholas Boylan <nboylan1pc@gmail.com>
2018-06-29 11:34:02 -0400

    reinstall react-datetime

--- client/src/components/student/Sessions.jsx
+++ client/src/components/student/Sessions.jsx
@@ -16,38 +16,38 @@ class Sessions extends Component {
   }
 
   getUpcomingSessions() {
-      // id will come from state or this.props.match.params
-      console.log(`Axios request to get upcoming sessions`)
-      // const {id} = this.props.match.params
-      // axios.get(`/sessions/${id}`)
-      // .then((resp) => {
-      //     this.setState({
-      //         sessions: resp.data
-      //     })
-      // })
+    // id will come from state or this.props.match.params
+    console.log(`Axios request to get upcoming sessions`)
+    // const {id} = this.props.match.params
+    // axios.get(`/sessions/${id}`)
+    // .then((resp) => {
+    //     this.setState({
+    //         sessions: resp.data
+    //     })
+    // })
   }
 
   deleteSession(id){
-      axios.delete(`/sessions/${id}`)
-      .then(() => {
-          this.getUpcomingSessions()
-      })
+    axios.delete(`/sessions/${id}`)
+    .then(() => {
+        this.getUpcomingSessions()
+    })
   }
 
   render() {
-      // import Link from react-router-dom and wrap around info.date or whatever we decide to put in there
-      // also wrap whatever we put in there with a button that also has access to the id of the session 
-      // put onClick => this.deleteSession
-      return (
-          <div>
-              <h1>Session Component!</h1> 
-              <ul>
-              {/* {this.state.sessions.map((info, index) => {
-                  return (<li key={index} >{info.date}</li>)
-              })} */}
-              </ul>
-          </div>
-      )
+    // import Link from react-router-dom and wrap around info.date or whatever we decide to put in there
+    // also wrap whatever we put in there with a button that also has access to the id of the session 
+    // put onClick => this.deleteSession
+    return (
+        <div>
+            <h1>Session Component!</h1> 
+            <ul>
+            {/* {this.state.sessions.map((info, index) => {
+                return (<li key={index} >{info.date}</li>)
+            })} */}
+            </ul>
+        </div>
+    )
   }
 }
 
