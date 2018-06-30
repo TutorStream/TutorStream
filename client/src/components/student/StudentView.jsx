2c02b4c1454d7c60e70c66944bd4fc4a5be59688
Nicholas Boylan <nboylan1pc@gmail.com>
2018-06-30 12:46:45 -0400

    initial css updates for flexbox

--- client/src/components/student/StudentView.jsx
+++ client/src/components/student/StudentView.jsx
@@ -91,59 +91,71 @@ class StudentView extends React.Component {
   render() {
     // console.log('state test id and user id', [this.state.user_id, this.state.test_ID]);
     // console.log('this.state.Tutors', this.state.Tutors);
-    console.log(this.props)
+    console.log(this.state.Tutors);
     return (
       <div>
         <Router>
           <div> 
-              <ul>
-                <Navbar style={{ fontSize: `130%` }}>
-                  <Nav>
-                  <LinkContainer to={"/student"}>
-                    <NavItem>Home</NavItem>
-                  </LinkContainer>
-                  <LinkContainer to={`/sessions/${this.state.user_id}`}>
-                    <NavItem>Sessions</NavItem>
-                  </LinkContainer>
-                  <LinkContainer to="/classroom">
-                    <NavItem>ClassRoom</NavItem>
-                  </LinkContainer>
-                  <LinkContainer to="/becometutor">
-                    <NavItem>Become a Tutor</NavItem>
-                  </LinkContainer>
-                  <LinkContainer to="/settings">
-                    <NavItem>Settings</NavItem>
-                  </LinkContainer>
-                  <LinkContainer to="/logout">
-                    <NavItem>Logout</NavItem>
-                  </LinkContainer>
-                  </Nav>
-                </Navbar>
-              </ul>
-              {this.state.routes.map((route, index) => (
-                <Route
-                  key={index}
-                  path={route.path}
-                  exact={route.exact}
-                  render={(routerProps) => <route.main {...routerProps} setTestID={this.setTestID} user_id={this.state.user_id} onClick={() => { this.props.history.push(route.path) }}/> }
-                />
-              ))}
-              <div className="tutors">
+            <ul>
+              <Navbar style={{ fontSize: `130%` }}>
+                <Nav>
+                <LinkContainer to={"/student"}>
+                  <NavItem>Home</NavItem>
+                </LinkContainer>
+                <LinkContainer to={`/sessions/${this.state.user_id}`}>
+                  <NavItem>Sessions</NavItem>
+                </LinkContainer>
+                <LinkContainer to="/classroom">
+                  <NavItem>ClassRoom</NavItem>
+                </LinkContainer>
+                <LinkContainer to="/becometutor">
+                  <NavItem>Become a Tutor</NavItem>
+                </LinkContainer>
+                <LinkContainer to="/settings">
+                  <NavItem>Settings</NavItem>
+                </LinkContainer>
+                <LinkContainer to="/logout">
+                  <NavItem>Logout</NavItem>
+                </LinkContainer>
+                </Nav>
+              </Navbar>
+            </ul>
+            {this.state.routes.map((route, index) => (
+              <Route
+                key={index}
+                path={route.path}
+                exact={route.exact}
+                render={(routerProps) => <route.main {...routerProps} setTestID={this.setTestID} user_id={this.state.user_id} onClick={() => { this.props.history.push(route.path) }}/> }
+              />
+            ))}
+            <div className="tutors-container">
+              <div className="all-tutors">
                 <ul>
                 {this.state.Tutors.map((tutor, i) => {
-                  return <li onClick={()=>{this.grabTutorId(tutor.ID)}} key={i}><Link to={`/tutor/${tutor.ID}`}>{tutor.Name}</Link></li>
+                  return (
+                    <div className="indv-tutor" onClick={()=>{this.grabTutorId(tutor.ID)}} key={i}>
+                      <Link to={`/tutor/${tutor.ID}`}>
+                      {tutor.Name}
+                      </Link>
+                      {/*other tutor data to render */}
+                      <br></br>
+                        <div>{tutor.Bio}</div>
+                      <br></br>
+                    </div>
+                  )
                 })}
                 </ul>
               </div> 
-              <Route exact path ='/tutor/:ID' render = {(routerProps)=> {
-                return (
-                   <TutorProfile 
-                   tutor_id={this.state.tutorId} 
-                   user_id={this.state.user_id} 
-                   test_ID={this.state.test_ID} 
-                   {...routerProps}
-                   />
-                )}}/>
+            </div>
+            <Route exact path ='/tutor/:ID' render = {(routerProps)=> {
+              return (
+                  <TutorProfile 
+                  tutor_id={this.state.tutorId} 
+                  user_id={this.state.user_id} 
+                  test_ID={this.state.test_ID} 
+                  {...routerProps}
+                  />
+              )}}/>
           </div>
         </Router>
       </div>
