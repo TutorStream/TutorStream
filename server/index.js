d5808aa589e7d77a698da5c90115e354aca32a02
Nicholas Boylan <nboylan1pc@gmail.com>
2018-07-01 19:10:29 -0400

    First Check - express router modularized

--- server/index.js
+++ server/index.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 const db = require ('./../database');
 
-const router = require('./routes');
-// const usersRouter = require('./routes/usersRoutes');
-// const tutorsRouter = require('./routes/tutorsRoutes');
-// const testsRouter = require('./routes/testsRoutes');
-// const sessionsRouter = require('./routes/sessionsRoutes');
+// const router = require('./routes');
+const usersRouter = require('./routes/usersRoutes');
+const tutorsRouter = require('./routes/tutorsRoutes');
+const testsRouter = require('./routes/testsRoutes');
+const sessionsRouter = require('./routes/sessionsRoutes');
 // const feedbackRouter = require('./routes/feedbackRoutes');
 
 const port = process.env.PORT || 3000;
@@ -16,11 +16,11 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, './../client/dist')));
 
-app.use('/', router);
-// app.use('/users', usersRouter);
-// app.use('/tutors', tutorsRouter);
-// app.use('/tests', testsRouter);
-// app.use('/sessions', sessionsRouter);
+// app.use('/', router);
+app.use('/users', usersRouter);
+app.use('/tutors', tutorsRouter);
+app.use('/tests', testsRouter);
+app.use('/sessions', sessionsRouter);
 // app.use('/feedback', feedbackRouter);
 
 
--- server/routes/tutorsRoutes.js
+++ server/routes/tutorsRoutes.js
@@ -1,18 +1,18 @@
-// const tutorsRouter = require('express').Router();
+const tutorsRouter = require('express').Router();
 // const usersControllers = require('./../controllers/users');
-// const tutorsControllers = require('./../controllers/tutors');
+const tutorsControllers = require('./../controllers/tutors');
 
-// // tutorsRouter.get('/tutors', usersControllers.getTutors);
-// tutorsRouter.get('/', usersControllers.getTutors);
+// tutorsRouter.get('/tutors', usersControllers.getTutors);
+tutorsRouter.get('/', tutorsControllers.getTutors);
 
-// // tutorsRouter.get('/tutors/selectTutors', usersControllers.getTutors);
-// tutorsRouter.get('/selectTutors', usersControllers.getTutors);
+// tutorsRouter.get('/tutors/selectTutors', usersControllers.getTutors);
+tutorsRouter.get('/selectTutors', tutorsControllers.getTutors);
 
-// // tutorsRouter.get('/tutors/:id', usersControllers.getTutorProfile);
-// tutorsRouter.get('/:id', usersControllers.getTutorProfile);
+// tutorsRouter.get('/tutors/:id', usersControllers.getTutorProfile);
+tutorsRouter.get('/:id', tutorsControllers.getTutorProfile);
 
-// // tutorsRouter.post('/tutors/:id', usersControllers.addOrUpdateTutor);
-// tutorsRouter.post('/:id', usersControllers.addOrUpdateTutor);
+// tutorsRouter.post('/tutors/:id', usersControllers.addOrUpdateTutor);
+tutorsRouter.post('/:id', tutorsControllers.addOrUpdateTutor);
 
 
-// module.exports = tutorsRouter;
+module.exports = tutorsRouter;
