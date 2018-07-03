ed60de00226f44c145356bc0d8a4cb5e788163ef
Michael Cortez <mijcortez@gmail.com>
2018-07-02 21:04:55 -0400

    google auth

--- server/config/passport_setup.js
+++ server/config/passport_setup.js
@@ -1,6 +1,9 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20')
 const config = require('../../credentials.json')
+// import user model
+
+
 
 passport.use(
     new GoogleStrategy({
@@ -8,8 +11,13 @@ passport.use(
         callbackURL: '/auth/google/redirect',
         clientID: config.web.client_id,
         clientSecret: config.web.client_secret
-    }, (accessToken, refreshToken, profile, done) => {
+    }, () => {
         // passport callback function 
-        console.log('passport has been fired', done)
+        console.log('passport has been fired')
+
+        // check if user already exists in our db 
+            // if so
+        // do nothing, otherwise
+        // insert into to our database
     })
 )
--- server/index.js
+++ server/index.js
@@ -33,6 +33,20 @@ router.get('/google', passport.authenticate('google',{
   scope:['profile']
 }))
 
+// a.get('/auth/google/callback',
+//   passport.authenticate('google'), // complete the authenticate using the google strategy
+//   (err, req, res, next) => { // custom error handler to catch any errors, such as TokenError
+//     if (err.name === 'TokenError') {
+//      res.redirect('/auth/google'); // redirect them back to the login page
+//     } else {
+//      // Handle other errors here
+//     }
+//   },
+//   (req, res) => { // On success, redirect back to '/'
+//     res.redirect('/');
+//   }
+// );
+
 // callback route for google to redirect
 router.get('/auth/google/redirect', passport.authenticate('google') ,(req, res) => {
   res.send('You reached the response URI')
