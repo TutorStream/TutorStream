624c64e0ba74e9b4ef12653117b7c9176e8289dd
Hack Reactor Student <student@hackreactor.com>
2018-07-12 12:21:12 -0400

    styled find tutor component

--- client/src/components/protected/studentView/StudentView.jsx
+++ client/src/components/protected/studentView/StudentView.jsx
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import { Row, Col , Card,
+  CardImg,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  CardText,
+  Button} from 'reactstrap';
+import { PageHeader, Jumbotron } from 'react-bootstrap';
 import TestList from './../../un-protected/TestList.jsx';
 
 class StudentView extends Component {
@@ -64,34 +72,73 @@ class StudentView extends Component {
   render() {
     return (
       <div>
-        <div>
-          <div>
-            <TestList setTestid={this.setTestid} />
-          </div>
-          <div className="tutors-container">
-            <div className="all-tutors">
-              {this.state.tutors.map((tutor, i) => {
-                return (
-                  <div className="indv-tutor" key={i}>
-                    <Link to={`/tutors/${tutor.id}`}>
-                      <span className="tutor-name">{tutor.Name}</span>
-                    </Link>
-                    <br />
-                    <div>Bio: {tutor.Bio}</div>
-                    <br />
-                    <br />
-                    <div>Rating: {tutor.Rating}</div>
-                    <br />
-                    <br />
-                    <div>Price: ${tutor.Price} / hr</div>
-                    <br />
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-          <div />
+        <TestList setTestid={this.setTestid} />
+        <Jumbotron className="container">
+        <div className="row-background">
+   
         </div>
+        <hr className="my-2" />
+        <Row>
+        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        <p className="lead">
+        </p>
+        </Row>
+      </Jumbotron>
+        <Jumbotron className="container">
+            {/* {this.state.tutors.map((tutor, i) => {
+              return (
+                <div className="indv-tutor" key={i}>
+                  <Link to={`/tutors/${tutor.id}`}>
+                    <span className="tutor-name">{tutor.Name}</span>
+                  </Link>
+                  <br />
+                  <div>Bio: {tutor.Bio}</div>
+                  <br />
+                  <br />
+                  <div>Rating: {tutor.Rating}</div>
+                  <br />
+                  <br />
+                  <div>Price: ${tutor.Price} / hr</div>
+                  <br />
+                </div>
+              );
+            })} */}
+
+            <div className="main-info">
+            <h2>Featured Tutors:</h2>
+            <Row>
+              <br />
+              {this.state.tutors.map(tutor => (
+                <Col xs="6" sm="4" key={tutor.id}>
+                  <Link to={`/tutors/${tutor.id}`}>
+                    <div>
+                      <Card>
+                        <CardImg
+                          className="img-circle"
+                          top
+                          width="20%"
+                          src={
+                            tutor.photo ||
+                            'https://cdn-images-1.medium.com/max/1200/1*MccriYX-ciBniUzRKAUsAw.png'
+                          }
+                          alt="default picture"
+                        />
+                        <CardBody>
+                          <CardTitle>{tutor.Name}</CardTitle>
+                          <CardText>{tutor.Bio}</CardText>
+                          <CardSubtitle>{tutor.Rating}</CardSubtitle>
+                          <Button color="info" size="sm">
+                            See Profile
+                          </Button>
+                        </CardBody>
+                      </Card>
+                    </div>
+                  </Link>
+                </Col>
+              ))}
+            </Row>
+          </div>
+        </Jumbotron>
       </div>
     );
   }
