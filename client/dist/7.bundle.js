f27faf04bd0627cd64bee6cc8bb5b4267482953e
Hack Reactor Student <student@hackreactor.com>
2018-07-13 11:56:43 -0400

    fix get upcoming sessions for tutors

---
+++ client/dist/7.bundle.js
@@ -0,0 +1,14 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[7],{
+
+/***/ "./client/src/components/protected/studentView/TutorRegistration.jsx":
+/*!***************************************************************************!*\
+  !*** ./client/src/components/protected/studentView/TutorRegistration.jsx ***!
+  \***************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nSyntaxError: Unexpected token, expected ; (52:5)\\n\\n  50 |   handleChange = event => {\\n  51 |     this.setState({ [event.target.name]: event.target.value });\\n> 52 |     });\\n     |      ^\\n  53 |   };\\n  54 | \\n  55 |   handleSubmit = event => {\\n\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9jbGllbnQvc3JjL2NvbXBvbmVudHMvcHJvdGVjdGVkL3N0dWRlbnRWaWV3L1R1dG9yUmVnaXN0cmF0aW9uLmpzeC5qcyIsInNvdXJjZXMiOltdLCJtYXBwaW5ncyI6IiIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./client/src/components/protected/studentView/TutorRegistration.jsx\n");
+
+/***/ })
+
+}]);
